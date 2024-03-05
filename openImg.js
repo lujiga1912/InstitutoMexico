fa94f7cb4c01816a65e59ce2ea2555de663536de
lujiga1912 <162386991+lujiga1912@users.noreply.github.com>
2024-03-05 17:59:36 -0600

    Add files via upload

---
+++ openImg.js
@@ -0,0 +1,25 @@
+function openFachada() {
+        var source = "fachada.html";
+    window.open(source);
+ } 
+function openSalon() {
+    var source = "salones.html";
+window.open(source);
+ }
+ function openGimnasio() {
+    var source = "gimnasio.html";
+window.open(source);
+ }
+ function openBiblioteca() {
+    var source = "biblioteca.html";
+window.open(source);
+ }
+ function openLaboratorio() {
+    var source = "laboratorio.html";
+window.open(source);
+ }
+ function openCanchas() {
+    var source = "canchas.html";
+window.open(source);
+ }
+ 
